8335acaefc628a5e4a1b468e08596c2b21eabd50
Artem <jock.fromrussia@gmail.com>
2025-09-08 23:05:00 +0300

    added validation and find first positive num

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,41 @@ public class Main {
 
         System.out.print("Укажите размер массива: "); // добавить валидацию
         final int arraySize = in.nextInt();
+        in.nextLine();
 
-        final int[] enteredNumbers = new int[arraySize];
-        System.out.print("Введите массив целых чисел: "); // добавить валидацию
-        for (int i = 0; i < arraySize; i++) {
-            final int num = in.nextInt();
-            enteredNumbers[i] = num;
+        System.out.print("Введите массив целых чисел: ");
+        final String inputLine = in.nextLine();
+        final String[] inputArray = inputLine.split(" ");
+
+        try {
+            if (arraySize != inputArray.length) {
+                throw new Exception("ОШИБКА: Размер не совпадает с кол-вом введенных значений");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
+        final int[] numbers = new int[arraySize];
 
+        for (int i = 0; i < inputArray.length; i++) {
+            try {
+                numbers[i] = Integer.parseInt(inputArray[i]);
+//                throw new NumberFormatException("ОШИБКА: Размер не совпадает с кол-вом введенных значений");
+            } catch (NumberFormatException e) {
+                numbers[i] = 0;
+                System.out.println("Ошибка" + inputArray[i]);
+            }
+        }
 
+        int firstPositiveNum = numbers[0];
+        for (int i = 0; i < arraySize; i++) {
+            if (numbers[i] > 0) {
+                firstPositiveNum = numbers[i];
+                break;
+            }
+        }
+
+        System.out.println(firstPositiveNum);
 
 
         // n - это начальньное звено то есть вершина треугольника
