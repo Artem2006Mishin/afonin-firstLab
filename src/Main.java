15fe5283df302bb7e9523177699bf0983a176c6f
Artem <jock.fromrussia@gmail.com>
2025-09-19 23:45:11 +0300

    added documentaiton

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,50 @@
 /*
-* Мишин Артём
-* Бояркин Максим
-* 24ВП1
-* Вариант 9 ЛАба 1
-* Рассчет треугольника Паскаля для первого положительного числа в массиве
-* */
+Мишин Артём && Бояркин Максим
+группа: 24ВП1 && вариант: 9
+номер лабораторной работы: 1
+задание: расчёт треугольника Паскаля для первого положительного числа в массиве
+*/
+
+/*
+    System - класс, который обеспечивает взаимодействие пользователя с операционной системой.
+    System.in - это поле типа InputStream. System.out - это поле типа PrintStream.
+    Это поля, которые ссылаются на объекты потоков (streams).
+
+    print() - это метод, который принадлежит классу PrintStream, выводит строку.
+    println() - это метод, который выводит строку в консоль с переводом строки.
+
+    Scanner - это класс для считывания пользовательского ввода из консоли.
+    scanner - это экземпляр класса Scanner.
+    new - ключевое слово, которое создает экземпляр класса указанного типа, выделяет под него память.
+    scanner.nextLine() - метод класса Scanner, используется для считывания целой строки, введенной пользователем.
+    Возвращает объект типа String.
+
+    Integer - класс обертка над примитивным типом int, он нужен для работы с примитивами, как с объектами.
+    Integer.parseInt - это статические метод класса Integer, который нужен, чтобы преобразовать строку в число.
+
+    Exception - это класс, который описывает ошибку. Наследуется от Throwable.
+    NumberFormatException - ошибка преобразования строки в число (отдельный класс ошибки).
+    throw — это ключевое слово, используется, чтобы выбросить исключение.
+    getMessage() — это метод класса Throwable. Он возвращает сообщение ошибки, которое мы передали при
+    создании исключения через new Exception("сообщение")
+
+    try-catch — это конструкция, которая позволяет поймать ошибку и обработать её.
+    (Exception e) e - это параметр блока catch, он ссылается на объект исключения, который был выброшен внутри try.
+
+    .split() - это метод класса String, который разбивает строку на части (подстроки) по заданному разделителю и
+    возвращает массив строк.
+
+    break — это ключевое слово, которое используется для немедленного выхода из цикла или switch.
+
+*/
+
+
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         boolean isCorrectInput = false;
         int arraySize = 0;
@@ -21,7 +55,7 @@ public class Main {
             System.out.print("Укажите размер массива: ");
 
             try {
-                final int inputSize = Integer.parseInt(in.nextLine());
+                int inputSize = Integer.parseInt(scanner.nextLine());
                 if (inputSize <= 0) {
                     throw new Exception("ОШИБКА: Размер массива должен быть положительным");
                 }
@@ -37,7 +71,7 @@ public class Main {
         isCorrectInput = false;
         while  (!isCorrectInput) {
             System.out.print("Введите массив целых чисел: ");
-            final String inputLine = in.nextLine();
+            String inputLine = scanner.nextLine();
             inputArray = inputLine.split(" ");
 
             try {
@@ -63,13 +97,13 @@ public class Main {
             }
         }
 
-        final int[][] arrayPascalTriangle = new int[firstPositiveNum][];
+        int[][] arrayPascalTriangle = new int[firstPositiveNum][];
         for (int i = 0; i < firstPositiveNum; i++) {
             arrayPascalTriangle[i] = new int[i + 1];
             for (int j = 0; j < i + 1; j++) {
                 if (j != 0 && j != i) {
-                    final int leftNum = arrayPascalTriangle[i - 1][j - 1];
-                    final int rightNum = arrayPascalTriangle[i - 1][j];
+                    int leftNum = arrayPascalTriangle[i - 1][j - 1];
+                    int rightNum = arrayPascalTriangle[i - 1][j];
                     arrayPascalTriangle[i][j] = leftNum + rightNum;
                 } else {
                     arrayPascalTriangle[i][j] = 1;
